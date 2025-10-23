133811645987f046d614be7747c285c17fbc5201
Andly <andlybras.desenvolvedor@outlook.com>
2025-10-23 13:05:04 -0400

    feat: Save directory index to .txt file and store result in parent directory

--- executing_consolidation.py
+++ executing_consolidation.py
@@ -9,9 +9,23 @@ def executing_consolidation(clean_path_directory_to_consolidate, file_name):
     for index, item in enumerate(parent_directory_items, start=1):
         index_of_parent_directory_items[index] = item
 
-    print("\n              🟢 DIRECTORY INDEX:\n")
-    for key, value in index_of_parent_directory_items.items():
-        print(f"              🔸 {key} ➖ {value}")
+    save_path = os.path.join(clean_path_directory_to_consolidate, file_name)
+
+    with open(save_path, 'w', encoding='utf-8') as file:
+        
+        console_header = "\n              🟢 DIRECTORY INDEX:\n"
+        file_header = "--- DIRECTORY INDEX ---\n\n" 
+
+        print(console_header)
+        file.write(file_header)
+
+        for key, value in index_of_parent_directory_items.items():
+        
+            console_line = f"              🔸 {key} ➖ {value}"
+            file_line = f"{key} ➖ {value}\n" 
+
+            print(console_line)
+            file.write(file_line)
 
 
     print(f"\n                🟢 Consolidation executed successfully. The file '{file_name}' has been created.")
